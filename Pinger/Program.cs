c7be0a41f0bc51aa30f7a175e52e423dc34f5d15
Kevin Isom <kevin.isom@gmail.com>
2013-01-10 12:45:15 +1300

    added restsharp client that hits the main feed url

---
+++ Pinger/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace Pinger
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				Console.WriteLine("Get News Data");
+				var url = "http://newsaggr.apphb.com/feed/json";
+				try
+				{
+					var client = new RestClient("http://newsaggr.apphb.com");
+					var request = new RestRequest("feed/json", Method.GET);
+
+					var response = client.Execute(request);
+					var content = response.Content;
+					Console.WriteLine(content);
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine(e);
+				}
+
+				Thread.Sleep(90000);
+			}
+
+
+		}
+	}
+}
